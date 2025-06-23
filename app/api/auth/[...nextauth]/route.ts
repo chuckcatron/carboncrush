3f622b89d584c68388524630c7a9415959891872
Chuck Catron <charles.catron@gmail.com>
2025-06-23 13:34:27 -0400

    Fix NextAuth API Route for App Router

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,4 @@ import { authOptions } from '@/lib/auth';
 
 const handler = NextAuth(authOptions);
 
-export async function GET(request: Request, context: any) {
-  return handler(request, context);
-}
-
-export async function POST(request: Request, context: any) {
-  return handler(request, context);
-}
+export { handler as GET, handler as POST };

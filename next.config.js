1d590c07d11f1e0fdb25dfe2c00e0bef19d33c98
Chuck Catron <charles.catron@gmail.com>
2025-06-23 13:14:56 -0400

    Fix NextAuth Configuration Errors

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['bcryptjs'],
   },
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 };
 
 module.exports = nextConfig;
